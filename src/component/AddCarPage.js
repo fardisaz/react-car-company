258524d32a83693f78fa6023d5ae5bdbb7f678ac
fardisaz <amoozadeh73@yahoo.com>
2020-12-30 12:45:34 +0100

    writeup-adding

--- src/component/AddCarPage.js
+++ src/component/AddCarPage.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addCar } from "../actions/cars";
 import CarForm from "./CarForm";
 
-const AddCarPage = () => (
+const AddCarPage = (props) => (
   <div>
     <h1> Add Car</h1>
-    <CarForm />
+    <CarForm
+      onSubmit={(car) => {
+        props.dispatch(addCar(car));
+        // this is going to switch us over as if we clicked the link which means we are not going through the full page refresh.It is going to switch over using browser routing
+        props.history.push("/");
+      }}
+    />
   </div>
 );
 
-export default AddCarPage;
+export default connect()(AddCarPage);
